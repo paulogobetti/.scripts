e0a7b6183722bc026566ff237238d4f751e4df46
Paulo Gobetti <78759327+paulogobetti@users.noreply.github.com>
2023-07-30 12:47:12 -0300

    rAthena init script Debian 11

---
+++ ratehna-debian-11-docker.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Dedicated Server
+
+#apt install nano ufw docker.io docker-compose sudo screen
+
+# Dependências server
+#apt install git make libmariadb-dev libmariadbclient-dev libmariadbclient-dev-compat gcc g++ zlib1g-dev libpcre3-dev
+
---
+++ rathena-debian-11-dedicated.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Dedicated Server
+
+#apt install nano ufw sudo screen
+
+# Dependências server
+#apt install git make libmariadb-dev libmariadbclient-dev libmariadbclient-dev-compat gcc g++ zlib1g-dev libpcre3-dev
+
+# Download e atualização
+#git clone https://github.com/rathena/rathena.git ~/rAthena
+#git pull
+
+# Compilação
+#./configure
+#make clean
+#make server
+
+# Permissões
+#chmod a+x login-server && chmod a+x char-server && chmod a+x map-server && chmod a+x web-server
+
+# Recompilar
+#./configure && make clean && make server
+
+#
+#   Iniciar:   ./athena-start start
+#   Parar:     ./athena-start stop
+#   Reiniciar: ./athena-start restart
+#
